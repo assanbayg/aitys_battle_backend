b5c8c1b0dffd4ef83de8f9e38690b0e696fcba49
Gauhar Assanbay <asanbayg@gmail.com>
2023-07-19 15:11:20 +0600

    Search aitys by topic

--- app/debate/router/router_get_aitys.py
+++ app/debate/router/router_get_aitys.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from fastapi import Depends, status
+from fastapi import Depends, status, HTTPException
 
 from app.utils import AppModel
 
@@ -8,8 +8,14 @@ from ..service import Service, get_service
 from . import router
 
 
+class GetAitysRequest(AppModel):
+    topic: str
+    first_figure: str
+    second_figure: str
+
+
 @router.get("/{id: str}", status_code=status.HTTP_200_OK)
-def get_aitys(id: str, svc: Service = Depends(get_service)):
+def get_aitys_by_id(id: str, svc: Service = Depends(get_service)):
     aitys = svc.repository.get_aitys_by_id(id)
     return {
         "id": id,
@@ -17,3 +23,19 @@ def get_aitys(id: str, svc: Service = Depends(get_service)):
         "first_figure": aitys["first_figure"],
         "second_figure": aitys["second_figure"],
     }
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+def get_aitys(
+    # input: GetAitysRequest,
+    topic: str,
+    svc: Service = Depends(get_service),
+):
+    aitys_data = svc.repository.get_aitys(input)
+    if aitys_data:
+        return aitys_data["replies"]
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Aitys not found",
+        )
